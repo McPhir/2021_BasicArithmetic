34e379b8d8363d43e335f98517682c34f1439a5f
SimonPhillips1 <75336829+McPhir@users.noreply.github.com>
2021-07-27 22:40:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+"""n1 = int(input("enter a number\n"))
+n2 = int(input("enter another number\n"))
+nSum = n1 + n2
+s = input("hit enter to continue\n")
+print("sum is",nSum)
+print("difference is ", n1 - n2)
+print("Product is", n1 * n2)
+print("Ratio is ", n1 // n2)
+"""
+
+
+d = int(input("enter a number in days"))
+y = d / 365
+print(y)
+y = d // 365
+print(y)
